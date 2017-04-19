ec38728f7919e8b43a9575e0ba952eca1b1b6c42
tricinel <bogdan@blazardesign.com>
2017-04-19 19:48:48 +0200

    Refactor and add flow types

--- __tests__/helpers/password-test.js
+++ __tests__/helpers/password-test.js
@@ -1,30 +1,24 @@
+// @flow
+
+import { times, uniq, compose, length } from 'ramda';
+
 import { getRandomNumber, generatePassword } from '../../src/helpers/password';
 
-const R = require('ramda');
+const createArrayOfRandomNumbers = (min: number, max: number, count: number): Function =>
+  times((): number => getRandomNumber(min, max), count);
 
-// Get rid of this and refactor
-const repeat = (fn, arg1, arg2, count) => {
-  const arr = [];
-
-  for (let i = 0; i < count; i += 1) {
-    arr.push(fn(arg1, arg2));
-  }
-
-  return arr;
-};
+const countUniqueValuesInArray = compose(length, uniq, createArrayOfRandomNumbers);
 
 test('Generates a random number between two numbers', () => {
   // generates a number that is lower than max and higher than min
   expect(getRandomNumber(1, 5)).toBeGreaterThanOrEqual(1);
   expect(getRandomNumber(1, 5)).toBeLessThanOrEqual(5);
 
-  // for a maximum of 5, at least 3 numbers should be different
-  const arr = repeat(getRandomNumber, 1, 5, 5);
-  expect(R.uniq(arr).length).toBeGreaterThan(1);
+  // for a maximum of 5, at least 2 numbers should be different
+  expect(countUniqueValuesInArray(1, 5, 5)).toBeGreaterThanOrEqual(2);
 
   // for a maximum of 100, at least 50 numbers should be different
-  const arr2 = repeat(getRandomNumber, 1, 100, 100);
-  expect(R.uniq(arr2).length).toBeGreaterThan(49);
+  expect(countUniqueValuesInArray(1, 100, 100)).toBeGreaterThanOrEqual(50);
 });
 
 test('Generate a random password', () => {
