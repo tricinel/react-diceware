1466b134d99d78b083ec66f6b88b5f8799094cba
juniorg <jnr.gonzalez@gmail.com>
2017-04-20 13:56:50 +0200

    re-factored password generator function

--- src/helpers/password-generator.js
+++ src/helpers/password-generator.js
@@ -1,37 +1,35 @@
-import wordList from './word-list'
+import wordList from './word-list';
 
-const rollDice = () => {
-  let diceRolls = []
-  for (let i = 1; i <= 5; i++) {
-    const getRandomNum = (max) => {
-      let randomNum
-      if (window.crypto && window.crypto.getRandomValues) {
-        let array = new Uint32Array(1)
-        window.crypto.getRandomValues(array)
-        randomNum = array[0] % max
-      } else if (window.msCrypto && window.msCrypto.getRandomValues) {
-        let array = new Uint32Array(1)
-        window.msCrypto.getRandomValues(array)
-        randomNum = array[0] % max
-      } else {
-        // instead of error, introduce fallback using Math.random
-        throw new Error('Crypto method not available')
-      }
-      return randomNum + 1
-    }
-    diceRolls.push(getRandomNum(6))
+const getRandomNum = (max) => {
+  let randomNum;
+  const array = new Uint32Array(1);
+  const Crypto = window.crypto || window.msCrypto;
+  if (Crypto !== undefined) {
+    Crypto.getRandomValues(array);
+    randomNum = array[0] % max;
+  } else {
+    // instead of error, introduce fallback using Math.random
+    throw new Error('Crypto method not available');
   }
-  return diceRolls.join('')
-}
+  return randomNum + 1;
+};
+
+const rollDice = (count) => {
+  const diceRolls = [];
+  for (let i = 1; i <= count; i += 1) {
+    diceRolls.push(getRandomNum(6));
+  }
+  return diceRolls.join('');
+};
 
 const getPassword = (count) => {
-  let words = []
-  for (let i = 0; i < count; i++) {
+  const words = [];
+  for (let i = 0; i < count; i += 1) {
     words.push(
-      wordList[rollDice()]
-    )
+      wordList[rollDice(5)]
+    );
   }
-  return words.join(' ')
-}
+  return words.join(' ');
+};
 
-export default getPassword
+export default getPassword;
