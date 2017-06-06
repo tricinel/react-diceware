6c2f26c59201f0f9b0c95f16ded6cdc9cc172265
tricinel <bogdan@blazardesign.com>
2017-06-06 17:36:54 +0200

    Improve Footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Heading from './common/HeadingStyledComponent';
 import Paragraph from './common/ParagraphStyledComponent';
+
+const Wrapper = styled.div`
+  border-top: 1px solid #ddd;
+  margin: 20px 0;
+  padding: 20px 0;
+`;
 
 const FooterTitle = Heading.withComponent('h2').extend`
   font-size: 20px;
@@ -12,7 +19,7 @@ const FooterLink = Paragraph.withComponent('a').extend`
 `;
 
 const Footer = () => (
-  <div>
+  <Wrapper>
     <FooterTitle>What is Diceware</FooterTitle>
     <Paragraph>
       Diceware is a method to build long, strong, memorable
@@ -21,7 +28,7 @@ const Footer = () => (
     <FooterLink href="http://world.std.com/~reinhold/diceware.html">
       Find out more
     </FooterLink>
-  </div>
+  </Wrapper>
 );
 
 export default Footer;
