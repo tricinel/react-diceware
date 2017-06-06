5d13eb7d6c2134a7a0fe63a796adeb64bacf905c
tricinel <bogdan@blazardesign.com>
2017-06-06 17:35:35 +0200

    Improve the paragraph styles

--- src/components/common/ParagraphStyledComponent.jsx
+++ src/components/common/ParagraphStyledComponent.jsx
@@ -3,9 +3,11 @@
 import styled from 'styled-components';
 
 const Paragraph = styled.p`
+  color: #444;
   font-family: 'Lato', sans-serif;
   font-size: 16px;
-  color: #444;
+  line-height: 140%;
+  padding: 10px 20px;
 `;
 
 export default Paragraph;
